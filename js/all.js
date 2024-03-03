29eb827a50b2d8a2854580b3ab74083ea2d11ce2
Clara Cai <clara.cai@tpisoftware.com>
2024-03-03 16:22:23 +0800

    配合設計稿，修改為 .833 秒跑一格

--- js/all.js
+++ js/all.js
@@ -2,20 +2,29 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+let previousSeconds = 0;
+let previousMinutes = 0;
+
 setInterval(() => {
   const date = new Date();
-  const seconds = date.getSeconds();
-  const minutes = date.getMinutes();
+  let seconds = Math.round(date.getSeconds() / .833333333333333); // 每秒有 72 格，60/72 = .833333333333333
+  let minutes = Math.round(date.getMinutes() / .833333333333333);
   const hours = date.getHours();
 
-  const secondRotation = seconds * 6;
-  const minuteRotation = minutes * 6;
+  seconds = (previousSeconds === seconds) ? ++seconds : seconds;
+  minutes = (seconds === 71 && previousMinutes === minutes) ? ++minutes : minutes;
+
+  previousSeconds = seconds++;
+  previousMinutes = minutes++;
+
+  const secondRotation = seconds * 5;
+  const minuteRotation = minutes * 5;
   const hourRotation = hours * 30 + minutes / 2;
 
   secondHand.style = `transform: translate(-50%, -100%) rotate(${secondRotation}deg)`;
   minuteHand.style = `transform: translate(-50%, -95%) rotate(${minuteRotation}deg)`;
   hourHand.style = `transform: translate(-50%, -95%) rotate(${hourRotation}deg)`;
-}, 1000);
+}, 833);
 
 
 
